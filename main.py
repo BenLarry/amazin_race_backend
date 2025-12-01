702afd51906bd62b593ad837751b6ed16ebe1d94
BenLarry <4muki4@gmail.com>
2025-12-01 12:18:50 +0200

    CORS, Flask and dotenv configured. Database connection class with dotenv variables'

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-
-
-
+@app.route("/")
+def helloWorld():
+  return "Hello, cross-origin-world!"
 
 if __name__ == "__main__":
     app.run(use_reloader=True, host='127.0.0.1', port=3000)
