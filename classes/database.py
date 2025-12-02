8da44c42c4967f36cf7861df6c2414c48de78e36
AlexAaltonen <alex.aaltonen99@gmail.com>
2025-12-02 13:22:32 +0200

    tein airport luokan metodit

--- classes/database.py
+++ classes/database.py
@@ -6,14 +6,20 @@ load_dotenv()
 
 class Database:
     def __init__(self):
-        self.conn = mysql.connector.connect(
-            host= os.getenv("HOST"),
-            port=os.getenv("PORT"),
-            database=os.getenv("DATABASE"),
-            user=os.getenv("USER"),
-            password=os.getenv("PASSWORD"),
-            autocommit=True
-        )
+        try:
+            self.conn = mysql.connector.connect(
+                host= os.getenv("HOST"),
+                port=os.getenv("PORT"),
+                database=os.getenv("DATABASE"),
+                user=os.getenv("USER"),
+                password=os.getenv("PASSWORD"),
+                autocommit=True
+            )
+        except mysql.connector.errors.ProgrammingError as err:
+            print('--------------------------------------------')
+            print(err)
+            print('----------------------------------------------------------')
+        return
 
     def get_conn(self):
         return self.conn
