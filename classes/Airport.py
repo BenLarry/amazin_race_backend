9d67856caa7cd6ffb9cd161f13e10cf417d16454
BenLarry <4muki4@gmail.com>
2025-12-02 09:15:05 +0200

    backend classes structure

---
+++ classes/Airport.py
@@ -0,0 +1,12 @@
+class Airport():
+    def __init__(self):
+        pass
+
+    def set_airport_visited(self, ident):
+        pass
+
+    def set_airport_special(self):
+        pass
+
+    def get_airport(self):
+        pass
---
+++ classes/Game.py
@@ -0,0 +1,21 @@
+class Game():
+    def __init__(self):
+        pass
+
+    def create_game(self):
+        pass
+
+    def select_game_airports(self, continent):
+        pass
+
+    def select_game_questions(self):
+        pass
+
+    def select_game_continent(self):
+        pass
+
+    def set_game_state(self):
+        pass
+
+    def get_game(self, player_id):
+        pass
