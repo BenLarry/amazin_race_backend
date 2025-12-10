5bd7a70782014b5a28678bdc0e6bd22a7660b163
BenLarry <4muki4@gmail.com>
2025-12-10 10:05:16 +0200

    commented bug

--- classes/Game.py
+++ classes/Game.py
@@ -25,13 +25,14 @@ class Game():
             airport = Airport(self.game_ID)
             start_airport = airport.select_random_airport()
             airport.set_airport_visited(start_airport)
+            end_airport = airport.select_random_airport()
 
             sql = "UPDATE game SET start_airport = %s, end_airport = %s, player_airport = %s WHERE ID = %s"
             cursor.execute(sql, (start_airport, end_airport, start_airport, self.game_ID))
-            end_airport = airport.select_random_airport()
-
-            for i in range(5):
-                airport.set_airport_special(airport.select_random_airport())
+            
+            #BUGAA PELIN LUOMISEN
+            #for i in range(5):
+            #    airport.set_airport_special(airport.select_random_airport())
 
             return {
                 "ID": self.game_ID,
